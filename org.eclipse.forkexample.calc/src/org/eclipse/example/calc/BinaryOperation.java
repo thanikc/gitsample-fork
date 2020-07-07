29a335eb84523d2a510e8bbec782861b9bf9e74b
Thanik Cheowtirakul <thanik.cheowtirakul.EXTERN@fiduciagad.de>
2020-07-07 12:08:59 +0200

    feat: adds comment #123

--- org.eclipse.forkexample.calc/src/org/eclipse/example/calc/BinaryOperation.java
+++ org.eclipse.forkexample.calc/src/org/eclipse/example/calc/BinaryOperation.java
@@ -18,4 +18,6 @@ public interface BinaryOperation extends Operation {
 	 * @return result of binary operation
 	 */ 
 	public float perform(float arg1, float arg2);
+	
+	// Thanik hat hier was reingeschrieben
 }
